c5c6ee130f7699c79595f05d1a499d58bbc672db
Andrea Amorosi <dreamorosi@gmail.com>
2023-04-23 18:45:46 +0200

    chore: fix types for tests

--- tests/helpers/customMatchers.ts
+++ tests/helpers/customMatchers.ts
@@ -13,36 +13,28 @@ expect.extend({
       isNot ? ' not' : ''
     } receive an Error with cause '${expected}',`;
 
-    let pass = false;
     // If the received value is not an error, we can't check the cause
     if (!(received instanceof Error)) {
       return {
-        pass,
+        pass: false,
         message: () => `${messageSuffix} but got none`,
         expected: 'an error',
         actual: typeof received,
       };
     }
-    const cause = Object(received).hasOwnProperty('cause')
-      ? received['cause']
-      : undefined;
+    const { cause } = received;
     // If the error has no cause, we can't check it
     if (cause === undefined) {
       return {
-        pass,
+        pass: false,
         message: () => `${messageSuffix} but got an error without a cause`,
       };
     }
     // If the cause is an error, we check the message, otherwise we check the string
-    if (cause instanceof Error) {
-      pass = cause.message === expected;
-    } else if (typeof cause === 'string') {
-      pass = cause === expected;
-    }
     const actualCause = cause instanceof Error ? cause.message : cause;
 
     return {
-      pass,
+      pass: actualCause === expected,
       message: () => `${messageSuffix} but got ${actualCause}`,
       expected,
       actual: actualCause,
